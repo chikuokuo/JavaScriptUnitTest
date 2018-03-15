a95497da805369be6ea70f69d61b34c8ff81e1d3
Bell Chen Chi Kuo <chikuokuo@msn.com>
2018-03-15 22:06:20 +0800

    c

--- JavaScriptUnitTest/Scripts/Test/main.test.js
+++ JavaScriptUnitTest/Scripts/Test/main.test.js
@@ -32,26 +32,19 @@ describe('Main Test', () => {
         // 在每個 Test Case 結束後執行的程式碼區塊
     });
 
-    it('Input 123:456 Correct Score Output should 123456', () => {
+    it('Input 123:456 Correct Output should 123456', () => {
         console.log(main);
-        var result = main.FormatOddsType("123:456", "Correct Score");
+        var result = main.FormatOddsType("123:456", "Correct");
         console.log(result);
         expect(result).to.be.equal('123456');
     });
-
-    //it('Test mock localStorage', () => {
-    //    localStorage["PreviousStakes"] = 300;
-    //    //console.log(localStorage["test"]);
-
-    //    expect(main.GetStake('s')).to.be.equal('100');
-    //});
 
     it('Test stub function', () => {
         formatNumber = sinon.stub(common, "formatNumber");
         formatNumber.withArgs("").returns("300");
         formatNumber.withArgs("100").returns("1");
 
-        localStorage["PreviousStakes"] = "100";
+        localStorage["Previouss"] = "100";
 
         expect(main.GetStake('s')).to.be.equal('100');
 
